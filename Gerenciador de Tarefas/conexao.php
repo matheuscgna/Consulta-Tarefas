bc7bc639ce3a0d274208d4893718488825aa8a26
Matheus Caetano Gonçalves Nascimento Araújo <matheus.cga.nascimento@gmail.com>
2026-08-20 22:04:56 -0300

    Delete Gerenciador de Tarefas/conexao.php

--- Gerenciador de Tarefas/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Conexão orientada a objetos usando a classe nativa mysqli
-$mysqli = new mysqli("localhost", "root", "", "todo_db");
-
-// Verificação obrigatória de erro na conexão
-if ($mysqli->connect_errno) {
-    die("Falha na conexão com o banco de dados: " . $mysqli->connect_error);
-}
-?>
